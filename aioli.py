cd7b7c6d24b281ad440a3550cc0634a1740b2a20
Jan <jan@janwilts.com>
2017-06-15 09:38:05 +0200

    Seperated reference detection

--- aioli.py
+++ aioli.py
@@ -4,84 +4,36 @@ from camera import Camera
 from frame import Frame
 from shapeprocessor import *
 
-
+# Constants
 REQUIRED_WIDTH = 370
-REFERENCE_SIZE_MM = 100
 TRAY_SIZE = 50
-REFERENCE_COLOR_LOWER = [0, 0, 0]
-REFERENCE_COLOR_UPPER = [0, 0, 0]
+REFERENCE_COLOR_LOW = [0, 0, 0]
+REFERENCE_COLOR_UP = [0, 0, 0]
+REFERENCE_SIZE_MM = 100
 
-cameras = [Camera('USB Cam', 1)]
+cameras = [Camera('USB Cam', 1, 'top')]
 
-# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# cv2.namedWindow("controls", 0)
-#
-# markLowRed = 28
-# markLowGreen = 145
-# markLowBlue = 54
-# markUppRed = 90
-# markUppGreen = 208
-# markUppBlue = 176
-#
-# def setGreenr(x):
-#     global markLowRed
-#     markLowRed = x
-# def setGreeng(x):
-#     global markLowGreen
-#     markLowGreen = x
-# def setGreenb(x):
-#     global markLowBlue
-#     markLowBlue = x
-# def setGreenR(x):
-#     global markUppRed
-#     markUppRed = x
-# def setGreenG(x):
-#     global markUppGreen
-#     markUppGreen = x
-# def setGreenB(x):
-#     global markUppBlue
-#     markUppBlue = x
-
-# # create trackbar
-# cv2.createTrackbar('g: r', "controls", 0, 255, setGreenr)
-# cv2.createTrackbar('g: g', "controls", 0, 255, setGreeng)
-# cv2.createTrackbar('g: b', "controls", 0, 255, setGreenb)
-# cv2.createTrackbar('g: R', "controls", 0, 255, setGreenR)
-# cv2.createTrackbar('g: G', "controls", 0, 255, setGreenG)
-# cv2.createTrackbar('g: B', "controls", 0, 255, setGreenB)
-# # set trackbar position
-# cv2.setTrackbarPos('g: r', "controls", markLowRed)
-# cv2.setTrackbarPos('g: g', "controls", markLowGreen)
-# cv2.setTrackbarPos('g: b', "controls", markLowBlue)
-# cv2.setTrackbarPos('g: R', "controls", markUppRed)
-# cv2.setTrackbarPos('g: G', "controls", markUppGreen)
-# cv2.setTrackbarPos('g: B', "controls", markUppBlue)
-# # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 def main():
-    # Loop through cameras
-    for cam in cameras:
-        image = Frame(cam.snap())
-
-        # Get the difference between ellipse and circle on reference
-        reference = Frame(cam.snap_color(REFERENCE_COLOR_LOWER, REFERENCE_COLOR_UPPER))
-        reference_px_mm, reference_diff = reference.find_reference(REFERENCE_SIZE_MM)
+    """ Main, function, entry point """
+    for camera in cameras:
+        image = Frame(camera.snap())
+        reference_px_mm, reference_diff = camera.calibrate(REFERENCE_COLOR_LOW, REFERENCE_COLOR_UP, REFERENCE_SIZE_MM)
 
         # Snap a frame ad process it, then find the contours
-        processed = Frame(cam.snap_canny(image.frame))
+        processed = Frame(camera.snap_canny(image.frame))
         lines = cv2.HoughLinesP(processed.frame, 1, np.pi / 180, 20)
 
         if lines is None:
-            # If the outer frame is not found
+            # TODO: Gui error here
             cv2.imshow(cam.name, image.frame)
             continue
 
+        # Return the cropped frame
         frame_crop = crop(image, lines, TRAY_SIZE)
 
         cv2.imshow('crop', frame_crop)
         cv2.imshow(cam.name, image.frame)
-        # cv2.imshow(cam.name + 'color', cam.snap_color([markLowBlue, markLowGreen, markLowRed], [markUppBlue, markUppGreen, markUppRed]))
-
 
 if __name__ == '__main__':
     for cam in cameras:
--- crop.py
+++ crop.py
@@ -7,33 +7,31 @@ lowest_line = None
 
 
 def crop(image, lines, tray_size):
-    """ Takes in an image, hough lines, and the width of the tray and returns a cropped image """
+    """ Takes in an image, Hough-Lines, and the width of the tray and returns a cropped image """
     global current_crop
     global highest_line
     global lowest_line
 
-    height, width, _ = image.frame.shape
+    image_h, image_w, _ = image.frame.shape
 
     if current_crop is None:
         current_crop = image.frame
 
     if highest_line is None and lowest_line is None:
-        highest_line = [0, height / 2, width, height / 2]
+        highest_line = [0, image_h / 2, image_w, image_h / 2]
         lowest_line = highest_line
 
     for x1, y1, x2, y2 in lines[0]:
-        line = [0, y1, width, y2]
+        line = [0, y1, image_w, y2]
 
         if lowest_line[1] > y1 and lowest_line[3] > y2:
             lowest_line = line
         elif highest_line[1] < y1 and highest_line[3] < y2:
             highest_line = line
 
-        cv2.line(image.frame, (0, y1), (width, y2), (0, 0, 255), 2)
+        cv2.line(image.frame, (0, y1), (image_w, y2), (0, 0, 255), 2)
 
     top_crop = (highest_line[1] + highest_line[3]) / 2 - tray_size
     bottom_crop = (lowest_line[1] + lowest_line[3]) / 2 + tray_size
 
-    current = image.frame[bottom_crop:top_crop, 0:]
-
-    return current
+    return image.frame[bottom_crop:top_crop, 0:]
